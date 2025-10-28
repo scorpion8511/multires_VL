e0137fbcd82e41da47517f04ac7d888cdf3ae6c5
scorpion8511 <dewan8511@gmail.com>
2025-10-28 13:37:21 +0900

    Normalize in situ carcinoma display label

--- scripts/sample_tcga_multiscale_patches.py
+++ scripts/sample_tcga_multiscale_patches.py
@@ -69,8 +69,8 @@ class ValueLabelEncoder:
     DEFAULT_MAPPING = {
         "normal": (0, "Normal"),
         "benign": (1, "Benign"),
-        "in situ carcinoma": (2, "in situ carcinoma"),
-        "carcinoma in situ": (2, "in situ carcinoma"),
+        "in situ carcinoma": (2, "In situ carcinoma"),
+        "carcinoma in situ": (2, "In situ carcinoma"),
         "invasive carcinoma": (3, "Invasive carcinoma"),
     }
 
